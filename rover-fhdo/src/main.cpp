dabcf9fef65fd60e8b6e1b1db100f5a3326af87f
Lamnn16 <nhatlamnguyen.vn@gmail.com>
2024-01-18 22:18:43 +0100

    Update code for milestone 2
    Signed-off-by: Lamnn16 <nhatlamnguyen.vn@gmail.com>

--- rover-fhdo/src/main.cpp
+++ rover-fhdo/src/main.cpp
@@ -4,23 +4,23 @@
 #include "collision.h"
 #include "motorDriver.h"
 
-void taskOne(void *parameter);
-void taskTwo(void *parameter);
-#define LED_BOARD 2 // change here the pin of the board to V2
+#define LED_BOARD 2 
+
+void taskBlinkLed(void *parameter);
+void taskSensorDataGet(void *parameter);
+void taskMotorControl(void *parameter);
 
 Collision collisionSensor;
 mclass motorDriver;
 
+bool obstacleDetected = false;
 
 void setup()
 {
   pinMode(LED_BOARD, OUTPUT);
   Serial.begin(9600);
-  // sensorObject.SETUP();
 
   motorDriver.SETUP();
-  motorDriver.motor_direction(MotorA, Backward);
-  motorDriver.set_speed(MotorA, Backward, 200);
 
   if (collisionSensor.init())
   {
@@ -36,94 +36,85 @@ void setup()
   delay(1000);
 
   xTaskCreate(
-      taskOne,   /* Task function. */
-      "TaskOne", /* String with name of task. */
-      5096,      /* Stack size in bytes. */
+      taskBlinkLed,   /* Task function. */
+      "TaskBlinkLed", /* String with name of task. */
+      7644,      /* Stack size in bytes. */
       NULL,      /* Parameter passed as input of the task */
       1,         /* Priority of the task. */
       NULL);     /* Task handle. */
 
   xTaskCreate(
-      taskTwo,   /* Task function. */
-      "TaskTwo", /* String with name of task. */
-      5096,      /* Stack size in bytes. */
+      taskSensorDataGet,   /* Task function. */
+      "TaskSensorDataGet", /* String with name of task. */
+      7644,      /* Stack size in bytes. */
       NULL,      /* Parameter passed as input of the task */
       1,         /* Priority of the task. */
       NULL);     /* Task handle. */
+
+  xTaskCreate(
+      taskMotorControl,   /* Task function. */
+      "taskMotorControl", /* String with name of task. */
+      7644,        /* Stack size in bytes. */
+      NULL,        /* Parameter passed as input of the task */
+      1,           /* Priority of the task. */
+      NULL);       /* Task handle. */
 }
 
 void loop()
 {
-
-  // Your loop code here
   delay(1000);
 }
 
-void taskOne(void *parameter)
+void taskBlinkLed(void *parameter)
 {
-  // example of a task that executes for some time and then is deleted
   for (;;)
   {
-    // Code section for getting data from AWS
-    
-    
-    // The blink led code below just be used for simulating an example task
-    // Switch on the LED
     digitalWrite(LED_BOARD, HIGH);
-    // Pause the task for 1000ms
-    // delay(1000); //This delay doesn't give a chance to the other tasks to execute
-    vTaskDelay(500 / portTICK_PERIOD_MS); // this pauses the task, so others can execute
-    // Switch off the LED
+    vTaskDelay(500 / portTICK_PERIOD_MS); 
     digitalWrite(LED_BOARD, LOW);
-    // Pause the task again for 500ms
     vTaskDelay(500 / portTICK_PERIOD_MS);
   }
-
   vTaskDelete(NULL);
 }
 
-void taskTwo(void *parameter)
+void taskSensorDataGet(void *parameter)
 {
-  // create an endless loop so the task executes forever
+  Collision::segments segments;
   for (;;)
   {
-
-    Collision::segments segments = collisionSensor.warning();
-
+    segments = collisionSensor.warning();
     if (segments.l)
     {
       Serial.println("Left collision detected!");
-      // Control motor here. For example:
-      // motorDriver.motor_direction(MotorA, Backward);
-      // motorDriver.motor_direction(MotorB, Forward);
+      motorDriver.motor_all_stop();
     }
-
     if (segments.c)
     {
-
       Serial.println("Center collision detected!");
-      int16_t cDistance = collisionSensor.c_distance();
-      Serial.print("Distance from center sensor: ");
-      Serial.print(cDistance);
-      Serial.println(" mm");
-      
-      // Control motor here. For example:
-      // motorDriver.motor_direction(MotorA, Backward);
-      // motorDriver.motor_direction(MotorB, Backward);
+      motorDriver.motor_all_stop();
     }
-
     if (segments.r)
     {
       Serial.println("Right collision detected!");
-      
-      // Control motor here. For example:
-      // motorDriver.motor_direction(MotorA, Forward);
-      // motorDriver.motor_direction(MotorB, Backward);
-      
+      motorDriver.motor_all_stop();
     }
-
-    delay(100); // Delay between readings
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
-  Serial.println("Ending task 2"); // should not reach this point but in case...
+  Serial.println("Ending task 2"); 
+  vTaskDelete(NULL);
+}
+
+void taskMotorControl(void *parameter)
+{
+  for (;;)
+  {
+    motorDriver.set_speed(MotorA, Forward, 80);
+    motorDriver.set_speed(MotorB, Forward, 80);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    motorDriver.set_speed(MotorA, Backward, 80);
+    motorDriver.set_speed(MotorB, Backward, 80);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+  Serial.println("Ending task 3");
   vTaskDelete(NULL);
 }
